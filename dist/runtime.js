5b3f760bd7711de1025e31465df5827591f7d814
Ories Zhu <tellt@126.com>
2019-11-29 16:22:26 +0800

    让我的申请页面最开始显示10条

--- dist/runtime.js
+++ dist/runtime.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "45b3b84dbcabebdb2158";
+/******/ 	var hotCurrentHash = "ab6960eef5c3d93edc55";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- src/views/MyApproval/index.vue
+++ src/views/MyApproval/index.vue
@@ -64,7 +64,7 @@
                 <p>【审核人】{{ step.emp_name }}</p>
                 <p>
                   【操作人】{{
-                    step.auditor_name ? step.auditor_name : "等待审核"
+                  step.auditor_name ? step.auditor_name : "等待审核"
                   }}
                 </p>
                 <p>【批复内容】{{ step.content }}</p>
@@ -79,26 +79,26 @@
         <span class="demonstration"></span>
         <el-pagination
           @size-change="handleSizeChange"
-         @current-change="handleCurrentChange"
+          @current-change="handleCurrentChange"
           :current-page="currentPage"
           :page-sizes="[10, 20, 100]"
           :page-size="pageSize"
           layout="total, sizes, prev, pager, next, jumper"
-          :total="totalTableNumber">
-        </el-pagination>
+          :total="totalTableNumber"
+        ></el-pagination>
       </div>
 
       <!--<div class="block">-->
-        <!--<span class="demonstration"></span>-->
-        <!--<el-pagination-->
-          <!--@size-change="handleSizeChange"-->
-          <!--@current-change="handleCurrentChange"-->
-          <!--:current-page="currentPage"-->
-          <!--:page-sizes="[10, 20, 100]"-->
-          <!--:page-size="10"-->
-          <!--layout="total, sizes, prev, pager, next, jumper"-->
-          <!--:total="totalTableNumber">-->
-        <!--</el-pagination>-->
+      <!--<span class="demonstration"></span>-->
+      <!--<el-pagination-->
+      <!--@size-change="handleSizeChange"-->
+      <!--@current-change="handleCurrentChange"-->
+      <!--:current-page="currentPage"-->
+      <!--:page-sizes="[10, 20, 100]"-->
+      <!--:page-size="10"-->
+      <!--layout="total, sizes, prev, pager, next, jumper"-->
+      <!--:total="totalTableNumber">-->
+      <!--</el-pagination>-->
       <!--</div>-->
 
       <h2>已审批</h2>
@@ -129,10 +129,9 @@
           :page-sizes="[10, 20, 100]"
           :page-size="pageSize1"
           layout="total, sizes, prev, pager, next, jumper"
-          :total="finishedTableNumber">
-        </el-pagination>
+          :total="finishedTableNumber"
+        ></el-pagination>
       </div>
-
     </div>
   </div>
 </template>
@@ -144,10 +143,10 @@ import {
   resolvedApproval,
   rejectedApproval,
   getUserFlows
-} from '@/api/MyApproval.js'
+} from "@/api/MyApproval.js";
 // import { getTotalAttendance } from "@/api/totalAttendance";
 
-import store from '@/store'
+import store from "@/store";
 export default {
   data() {
     return {
@@ -155,154 +154,156 @@ export default {
       ApprovalTable: [],
       myApplicationTable: [],
       finishApplicationTable: [],
-      myfinishApplicationTable:[],
+      myfinishApplicationTable: [],
       initDataloding: false,
       currentPage: 1,
-      currentPage1:1,
-      finishedCurrentPage :1,
-      finishedPagesize:[15,25,80],
+      currentPage1: 1,
+      finishedCurrentPage: 1,
+      finishedPagesize: [15, 25, 80],
       pageSize: 10,
       pageSize1: 10,
       dialogTableVisible: false,
       gridData: [],
-      processTitle: '',
-      content: '',
-      processID: '',
+      processTitle: "",
+      content: "",
+      processID: "",
       dialogTableVisible1: false,
       processApprove: [],
       initDataList: [],
-      currentPage4:1
-    }
+      currentPage4: 1
+    };
   },
 
   computed: {
     totalTableNumber() {
-      return this.ApprovalTable.length
+      return this.ApprovalTable.length;
     },
     finishedTableNumber() {
-      return this.finishApplicationTable.length
+      return this.finishApplicationTable.length;
     }
   },
   watch: {},
 
   created() {
-    this.initDataloding = true
-    this.getApprovals()
-    this.initGetUserFlows()
-    this.initDataloding = false
+    this.initDataloding = true;
+    this.getApprovals();
+    this.initGetUserFlows();
+    this.initDataloding = false;
   },
 
   methods: {
-
     initGetUserFlows() {
-      getUserFlows({ access_token: this.access_token }).then((res) => {
-        const dataObj = res.data.handle_procs || null
-        const arrayObj = []
-        Object.values(dataObj).forEach((item) => {
-          arrayObj.push(item)
-        })
+      getUserFlows({ access_token: this.access_token }).then(res => {
+        const dataObj = res.data.handle_procs || null;
+        const arrayObj = [];
+        Object.values(dataObj).forEach(item => {
+          arrayObj.push(item);
+        });
         const statusMap = {
-          '0': '进行中',
-          '9': '通过',
-          '-1': '驳回',
-          '-2': '已撤销',
-          '-9': '草稿'
-        }
+          "0": "进行中",
+          "9": "通过",
+          "-1": "驳回",
+          "-2": "已撤销",
+          "-9": "草稿"
+        };
         for (const i of arrayObj) {
           const obj = {
-            flowName: '',
-            position: '',
-            name: '',
-            title: '',
-            createAt: '',
-            status: ''
-          }
-          const emp = 'emp'
-          obj.flowName = i[0].entry.title
-          obj.position = i[0].entry[emp].position
-          obj.name = i[0].entry[emp].name
-          obj.title = i[0].entry.title
-          obj.createAt = i[0].created_at
-          obj.status = statusMap[i[0].status]
-          this.finishApplicationTable.push(obj)
+            flowName: "",
+            position: "",
+            name: "",
+            title: "",
+            createAt: "",
+            status: ""
+          };
+          const emp = "emp";
+          obj.flowName = i[0].entry.title;
+          obj.position = i[0].entry[emp].position;
+          obj.name = i[0].entry[emp].name;
+          obj.title = i[0].entry.title;
+          obj.createAt = i[0].created_at;
+          obj.status = statusMap[i[0].status];
+          this.finishApplicationTable.push(obj);
         }
-        this.myfinishApplicationTable = this.finishApplicationTable
-        this.loading = false
-      })
+        this.myfinishApplicationTable = this.finishApplicationTable.slice(
+          0,
+          10
+        );
+        this.loading = false;
+      });
     },
 
     nextStep() {
-      this.$confirm('将通过进入下一步, 是否继续?', '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'info'
+      this.$confirm("将通过进入下一步, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "info"
       })
         .then(() => {
           const obj = {
             access_token: this.access_token,
             process_id: this.processID,
             content: this.content
-          }
+          };
           resolvedApproval(obj).then(resolve => {
             // this.$alert("通过成功");
             this.$message({
-              type: 'success',
-              message: '通过成功!'
-            })
-          })
-          this.dialogTableVisible = false
-          location.reload()
+              type: "success",
+              message: "通过成功!"
+            });
+          });
+          this.dialogTableVisible = false;
+          location.reload();
         })
         .catch(() => {
           this.$message({
-            type: 'info',
-            message: '已取消'
-          })
-        })
+            type: "info",
+            message: "已取消"
+          });
+        });
     },
     reject() {
-      this.$confirm('将驳回, 是否继续?', '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'info'
+      this.$confirm("将驳回, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "info"
       })
         .then(() => {
           const obj = {
             access_token: this.access_token,
             process_id: this.processID,
             content: this.content
-          }
-          console.log(obj)
-          this.loading = true
+          };
+          console.log(obj);
+          this.loading = true;
           rejectedApproval(obj).then(resolve => {
-            this.$alert('驳回成功')
-            this.loading = false
-          })
-          this.dialogTableVisible = false
-          location.reload()
+            this.$alert("驳回成功");
+            this.loading = false;
+          });
+          this.dialogTableVisible = false;
+          location.reload();
         })
         .catch(() => {
           this.$message({
-            type: 'info',
-            message: '已取消'
-          })
-        })
+            type: "info",
+            message: "已取消"
+          });
+        });
     },
     handleEdit(index, row) {
-      this.processID = row.passid
+      this.processID = row.passid;
       // console.log(this.access_token);
       console.log({
         access_token: this.access_token,
         entry_id: row.id
-      })
-      this.dialogTableVisible = true
-      this.loading = true
+      });
+      this.dialogTableVisible = true;
+      this.loading = true;
       getEntryDetail({
         access_token: this.access_token,
         entry_id: row.id
       }).then(res => {
-        this.processTitle = row.title
-        console.log(res)
+        this.processTitle = row.title;
+        console.log(res);
         // // this.initEntryDetailData = res.data.entry;
         // this.templateFormsData = [];
         // this.templateFormsData = res.data.template_forms;
@@ -314,114 +315,114 @@ export default {
         //   this.gridData.push(obj);
         // }
         // this.loading = false;
-        this.templateFormsData = []
-        this.templateFormsData = res.data.template_forms
-        this.gridData = []
+        this.templateFormsData = [];
+        this.templateFormsData = res.data.template_forms;
+        this.gridData = [];
         for (const i of this.templateFormsData) {
-          const obj = {}
-          obj.name = i.field_name
-          obj.value = i.field_value
-          this.gridData.push(obj)
+          const obj = {};
+          obj.name = i.field_name;
+          obj.value = i.field_value;
+          this.gridData.push(obj);
         }
-        this.loading = false
-      })
+        this.loading = false;
+      });
     },
     handleSizeChange(val) {
-      this.pageSize = val
+      this.pageSize = val;
       this.myApplicationTable = this.ApprovalTable.slice(
         (this.currentPage - 1) * this.pageSize,
         this.currentPage * this.pageSize
-      )
+      );
     },
     handleSizeChange1(val) {
-      console.log("进入页面改变"+val)
-      this.pageSize1 = val
+      console.log("进入页面改变" + val);
+      this.pageSize1 = val;
       this.myfinishApplicationTable = this.finishApplicationTable.slice(
         (this.currentPage1 - 1) * this.pageSize1,
         this.currentPage1 * this.pageSize1
-      )
-      console.log(this.myfinishApplicationTable)
+      );
+      console.log(this.myfinishApplicationTable);
     },
     handleCurrentChange(val) {
-      this.currentPage = val
+      this.currentPage = val;
       this.myApplicationTable = this.ApprovalTable.slice(
         (val - 1) * this.pageSize,
         val * this.pageSize
-      )
+      );
     },
     handleCurrentChange1(val) {
-      this.currentPage1 = val
+      this.currentPage1 = val;
       this.myfinishApplicationTable = this.finishApplicationTable.slice(
         (val - 1) * this.pageSize1,
         val * this.pageSize1
-      )
+      );
     },
 
     getApprovals() {
       getUserFlowsApprovals({ access_token: this.access_token }).then(res => {
-        console.log(res)
-        const list = res.data.procs
+        console.log(res);
+        const list = res.data.procs;
         const statusMap = {
-          '0': '进行中',
-          '9': '通过',
-          '-1': '驳回',
-          '-2': '已撤销',
-          '-9': '草稿'
-        }
+          "0": "进行中",
+          "9": "通过",
+          "-1": "驳回",
+          "-2": "已撤销",
+          "-9": "草稿"
+        };
         for (const i of list) {
           const obj = {
-            flowName: '',
-            position: '',
-            name: '',
-            title: '',
-            createAt: '',
-            status: ''
-          }
+            flowName: "",
+            position: "",
+            name: "",
+            title: "",
+            createAt: "",
+            status: ""
+          };
 
-          obj.flowName = i.flow.flow_name
-          obj.position = i.entry.emp.position
-          obj.name = i.entry.emp.name
-          obj.title = i.entry.title
-          obj.createAt = i.created_at
-          obj.status = statusMap[i.status]
-          obj.id = i.entry_id
-          obj.passid = i.id
-          this.ApprovalTable.push(obj)
+          obj.flowName = i.flow.flow_name;
+          obj.position = i.entry.emp.position;
+          obj.name = i.entry.emp.name;
+          obj.title = i.entry.title;
+          obj.createAt = i.created_at;
+          obj.status = statusMap[i.status];
+          obj.id = i.entry_id;
+          obj.passid = i.id;
+          this.ApprovalTable.push(obj);
         }
-        this.myApplicationTable = this.ApprovalTable
-        this.loading = false
-      })
+        this.myApplicationTable = this.ApprovalTable.slice(0, 10);
+        this.loading = false;
+      });
     },
     handleProcess(index, row) {
-      console.log(index, row)
-      this.dialogTableVisible1 = true
-      this.loading = true
-      this.processTitle = row.title
+      console.log(index, row);
+      this.dialogTableVisible1 = true;
+      this.loading = true;
+      this.processTitle = row.title;
       getEntryDetail({
         access_token: this.accessToken,
         entry_id: row.id
       }).then(res => {
-        console.log(res)
+        console.log(res);
         // this.initEntryDetailData = res.data.entry;
-        this.processApprove = []
-        this.gridData1 = []
+        this.processApprove = [];
+        this.gridData1 = [];
         for (const i of res.data.procs) {
-          const obj = {}
-          obj.process_name = i.process_name
-          obj.status = i.status
-          obj.emp_name = i.emp_name
-          obj.auditor_name = i.auditor_name
-          obj.content = i.content
-          obj.updated_at = i.updated_at
-          this.processApprove.push(obj)
+          const obj = {};
+          obj.process_name = i.process_name;
+          obj.status = i.status;
+          obj.emp_name = i.emp_name;
+          obj.auditor_name = i.auditor_name;
+          obj.content = i.content;
+          obj.updated_at = i.updated_at;
+          this.processApprove.push(obj);
         }
-        this.processApprove = this.processApprove.reverse()
-        this.loading = false
-      })
-      this.loading = false
+        this.processApprove = this.processApprove.reverse();
+        this.loading = false;
+      });
+      this.loading = false;
     }
   }
-}
+};
 </script>
 
 <style  scoped>
