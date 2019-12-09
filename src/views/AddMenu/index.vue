7a051ae0dfc3a450f66eb459cf98d47421dd00dd
Ories Zhu <tellt@126.com>
2019-12-09 15:21:08 +0800

    12-09

--- src/views/AddMenu/index.vue
+++ src/views/AddMenu/index.vue
@@ -5,8 +5,8 @@
       style="width: 100%;margin-bottom: 20px;"
       row-key="id"
       border
-      default-expand-all
       :tree-props="{children: 'children', hasChildren: 'hasChildren'}"
+      :default-expand-all="true"
     >
       <el-table-column
         prop="title"
@@ -53,18 +53,18 @@
           <el-button
             size="mini"
             type="primary"
-            @click="handleUp(scope.$index, scope.row)"
             circle
+            @click="handleUp(scope.$index, scope.row)"
           >
-            <i class="el-icon-arrow-up"></i>
+            <i class="el-icon-arrow-up" />
           </el-button>
           <el-button
             size="mini"
             type="success"
-            @click="handleDelete(scope.$index, scope.row)"
             circle
+            @click="handleDown(scope.$index, scope.row)"
           >
-            <i class="el-icon-arrow-down"></i>
+            <i class="el-icon-arrow-down" />
           </el-button>
         </template>
       </el-table-column>
@@ -80,6 +80,7 @@ import store from '@/store'
 export default {
   data() {
     return {
+      Expand: true,
       menuTable: [],
       tree: 'home',
       options: [],
@@ -105,9 +106,62 @@ export default {
   },
 
   methods: {
-    handleUp(a,b){
-      console.log(a)
-      console.log(b)
+    findLayer(title) { // 找到点的是第几层的第几个
+      const index = this.menuTable.findIndex((e) => { return e['title'] === title })
+      if (index !== -1) {
+        return [index, -1]
+      } else {
+        for (let i = 0; i < this.menuTable.length; i++) {
+          for (let j = 0; j < this.menuTable[i].children.length; j++) {
+            if (this.menuTable[i].children[j].title === title) {
+              return ([i, j, this.menuTable[i].children.length])
+            }
+          }
+        }
+      }
+    },
+    handleUp(a, b) {
+      const title = b.title
+      const TemperMenuTable = this.menuTable
+      const Layer = this.findLayer(title)
+      const layerIndex1 = Layer.slice(0, 1)[0]
+      const layerIndex2 = Layer.slice(1, 2)[0]
+      if ((layerIndex1 !== 0) && (layerIndex2 === -1)) {
+        const index1 = layerIndex1
+        const index2 = layerIndex1 - 1
+        TemperMenuTable.splice(index2, 1, ...TemperMenuTable.splice(index1, 1, TemperMenuTable[index2]))
+      } else if ((layerIndex2 !== -1) && (layerIndex2 !== 0)) { // (layerIndex1 !== 0) &&
+        const index0 = layerIndex1
+        const index1 = layerIndex2
+        const index2 = layerIndex2 - 1
+        const TempList = TemperMenuTable.slice(index0, index0 + 1)[0].children
+        TempList.splice(index2, 1, ...TempList.splice(index1, 1, TempList[index2]))
+        const tempTable = this.menuTable[index0]
+        tempTable.children = TempList
+        this.$set(this.menuTable, index0, tempTable)
+      }
+    },
+    handleDown(a, b) {
+      const title = b.title
+      const TemperMenuTable = this.menuTable
+      const Layer = this.findLayer(title)
+      const layerIndex1 = Layer.slice(0, 1)[0]
+      const layerIndex2 = Layer.slice(1, 2)[0]
+      const length = Layer.slice(2, 3)[0]
+      if ((layerIndex1 !== this.menuTable.length - 1) && (layerIndex2 === -1)) {
+        const index1 = layerIndex1
+        const index2 = layerIndex1 + 1
+        TemperMenuTable.splice(index2, 1, ...TemperMenuTable.splice(index1, 1, TemperMenuTable[index2]))
+      } else if ((layerIndex2 !== -1) && (layerIndex2 < length - 1)) { // (layerIndex1 !== 0) &&
+        const index0 = layerIndex1
+        const index1 = layerIndex2
+        const index2 = layerIndex2 + 1
+        const TempList = TemperMenuTable.slice(index0, index0 + 1)[0].children
+        TempList.splice(index2, 1, ...TempList.splice(index1, 1, TempList[index2]))
+        const tempTable = this.menuTable[index0]
+        tempTable.children = TempList
+        this.$set(this.menuTable, index0, tempTable)
+      }
     },
     allowDrop(draggingNode, dropNode, type) {
       if (draggingNode.level === dropNode.level) {
